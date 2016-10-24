8a9c7c7a6899122729c14a8197c4a37ecf1415f5
Craig Coleman <craigcoleman@users.noreply.github.com>
2016-10-24 05:14:28 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>vita</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!--  There are no external links to css or js-->
+  <link rel="stylesheet" href="dist/css/bootstrap.min.css">
+   <link rel="stylesheet" href="dist/css/custom.css">
+  <script src="dist/jquery/jquery.min.js"></script>
+  <script src="dist/js/bootstrap.min.js"></script>
+</head>
+<body>
+<!--  add a menu here -->
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">vita</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="#">Home</a></li>
+		<!-- start dropdown -->
+			<li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Page 1 <span class="caret"></span></a>
+				<ul class="dropdown-menu">
+					<li><a href="#">Page 1-1</a></li>
+					<li><a href="#">Page 1-2</a></li>
+					<li><a href="#">Page 1-3</a></li>
+				</ul>
+			</li>
+		  <!-- start dropdown -->
+      <li><a href="phpcode/">phpcode</a></li>
+      <li><a href="#">Page 3</a></li>
+    </ul>
+  </div>
+</nav>
+  
+
+      <div class="container">
+			<div class="embed-responsive embed-responsive-16by9">
+				<iframe  class="embed-responsive-item" name="if" src="intro.html"  ></iframe>
+			</div>
+		</div>
+
+  
+</body>
+</html>
